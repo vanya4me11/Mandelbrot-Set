e27d07a0e2a1f640333cdb04509d3e014891a6bf
vanya4me11 <w2000092@apps.losrios.edu>
2025-04-30 14:17:20 -0700

    Added basics from assignment desc.

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -1,2 +1,12 @@
 #ifndef COMPLEXPLANE_H
 #define COMPLEXPLANE_H
+
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
+enum State {CALCULATING, DISPLAYING};
+
+
+
