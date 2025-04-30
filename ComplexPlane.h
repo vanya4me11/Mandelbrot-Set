3489a13bb6eb8d2b59318d4b29f0ab132374d3c9
Zerio105 <85314670+Zerio105@users.noreply.github.com>
2025-04-30 15:27:11 -0700

    Filled in ComplexPlane header declarations

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -13,3 +13,30 @@ enum State {CALCULATING, DISPLAYING};
 
 //TODO: Declare the rest of ComplexPlane as specified in the UML diagram
 
+class ComplexPlane
+{
+    public:
+        // Public functions
+        // There are some argument types that don't seem to have been defined?
+        ComplexPlane(int pixelWidth, int pixelHeight);
+        void draw(RenderTarget& target, RenderStates states) const;
+        void zoomIn();
+        void zoomOut();
+        void setCenter(Vector2i mousePixel);
+        void setMouseLocation(Vector2i mousePixel);
+        void loadText(Text& text);
+        void updateRender();
+    private:
+        // Private variables
+        VertexArray m_vArray;
+        State m_state;
+        Vector2f m_mouseLocation;
+        Vector2i m_pixel_size;
+        Vector2f m_plane_size;
+        int m_zoomCount;
+        float m_aspectRatio;
+        // Private functions
+        int countIterations(Vector2f coord);
+        void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+        Vector2f mapPixelToCoords(Vector2i mousePixel);
+};
