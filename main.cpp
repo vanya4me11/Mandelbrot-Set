f671a92033c6f4d110190895f828dac00d082ee3
vanya4me11 <w2000092@apps.losrios.edu>
2025-04-30 14:39:48 -0700

    Added comments/clarity

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
-// abs complex example
+
+/*Get the desktop resolution using:
+    VideoMode::getDesktopMode().width;
+    VideoMode::getDesktopMode().height;
+    Note:  you may want to divide these values by 2 to start with a smaller screen so your program runs faster
+Construct the RenderWindow
+Construct the ComplexPlane
+Construct the Font and Text objects*/
+
+*/Begin the main loop
+    Handle Input segment
+        Poll Windows queue events
+            Handle Event::Closed event to close the window
+            Handle Event::MouseButtonPressed
+                Right click will zoomOut and call setCenter on the ComplexPlane object with the (x,y) pixel location of the mouse click
+                Left click will zoomIn and call setCenter on the ComplexPlane object with the (x,y) pixel location of the mouse click
+            Handle Event::MouseMoved
+                Call setMouseLocation on the ComplexPlane object to store the (x,y) pixel location of the mouse click
+                This will be used later to display the mouse coordinates as it moves
+        Check if Keyboard::isKeyPressed(Keyboard::Escape) to close the window
+    Update Scene segment
+        Call updateRender on the ComplexPlane object
+        Call loadText on the ComplexPlane object
+    Draw Scene segment
+        Clear the RenderWindow object
+        draw the ComplexPlane object
+        draw the Text object
+        Display the RenderWindow object*/
+
+
+//EVERYTHING BELOW HERE IS AN EXAMPLE DIRECTLY FROM PROJECT PAGE
+
 #include <iostream>     // std::cout
 #include <complex>      // std::complex, std::abs
-#include "ComplexPlane.h"
+#include "ComplexPlane.h"  //THIS LINE IS NOT FROM EXAMPLE; ADDED LATER.
 using namespace std;
 
 void testConvergence(complex<double> c)
